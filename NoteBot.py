879cc002a72d25e7cabcc59ca73523a9f3b2fa6e
Mishahal Palakuniyi <58310848+thebadcoder96@users.noreply.github.com>
2021-07-12 17:48:07 -0500

    Updated bot
    Added a way to connect using the "More" dropdown button if the normal way fails

--- NoteBot.py
+++ NoteBot.py
@@ -27,8 +27,8 @@ except:
 
 
 #Enter keywords and number of pages to visit
-keywords="Data Analysis,Data Engineering,Hiring,Data Science"
-pages = 100
+keywords="Enter Keywords here"
+pages = 100 #Enter number of pages to stop at
 
 
 
@@ -54,38 +54,58 @@ def getProfiles(soup, queued):
 
 for page in range(1, pages+1):
     #Searching for the industry to connect with people
-    driver.get("https://www.linkedin.com/search/results/people/?keywords="+keywords+"&origin=GLOBAL_SEARCH_HEADER&page="+str(page))
+    driver.get("https://www.linkedin.com/search/results/people/?keywords="+keywords+"&origin=CLUSTER_EXPANSION&page="+str(page))
     queued.extend(getProfiles(BeautifulSoup(driver.page_source), queued))
     
     print('Profiles Visited: ', len(visited))
-
+    
     while queued:
         try:
             visiting = queued.pop()
             visited.append(visiting)
             #visiting profile
             driver.get(visiting)
+            #getting the name of the profile
             name = driver.find_element_by_class_name("text-heading-xlarge").get_attribute('innerHTML')
-            message = "Hi "+name.split()[0]+"!\n\nEnter your personalized note"
-
+            
+            #ENTER THE PERSONALIZED NOTE
+            message = "Hi "+name.split()[0]+"!\n\n Enter you personalized note here."
+            
             #Connecting and sending the personalized note
             #driver.find_element_by_xpath('//button[text()="Connect"]').click()
             driver.find_element_by_class_name('pvs-profile-actions__action').click()
             time.sleep(5)
-            driver.find_element_by_class_name('artdeco-button--secondary').click()
-            driver.find_element_by_id('custom-message').send_keys(message)
-            time.sleep(3)
-            driver.find_element_by_class_name('ml1').click()
-
+            
+            #If there is error while connecting using the blue connect button, try to connect using the "More" dropdown button
+            try:
+                driver.find_element_by_class_name('artdeco-button--secondary').click()
+            except:
+                print("Error connecting with " + name + ". Trying with More button")
+                try:
+                    driver.find_element_by_xpath('/html/body/div[6]/div[3]/div/div/div/div/div[3]/div/div/main/div/section/div[2]/div[3]/div/div/button').click()
+                except:
+                    driver.find_element_by_xpath('/html/body/div[5]/div[3]/div/div/div/div/div[3]/div/div/main/div/section/div[2]/div[3]/div/div/button').click()
+                #driver.find_element_by_xpath('/html/body/div[6]/div[3]/div/div/div/div/div[3]/div/div/main/div/section/div[2]/div[3]/div/div/div/div/ul/li[4]/div').click()
+                finally:
+                    time.sleep(2)
+                    driver.find_element_by_xpath('//div[@data-control-name="connect"]').click()
+                    time.sleep(2)
+                    driver.find_element_by_class_name('mr2').click()
+                    driver.find_element_by_class_name('artdeco-button--secondary').click()
+            finally:
+                #Sending the message
+                driver.find_element_by_id('custom-message').send_keys(message)
+                time.sleep(3)
+                driver.find_element_by_class_name('ml1').click()
+                time.sleep(5)
+                
             #saving visited profile
             with open('visited.txt', 'a') as f:
-                f.write(str(visiting)+'\n')
+                f.write(str(visiting)+" ")
             f.close()
-            time.sleep(5)
-            
             
         except:
             print("Unable to connect with profile: " + name)
             
-print('Completed!! Profiles Connected with: ', len(visited))
+print('Completed!! Profiles Visited: ', len(visited))
 driver.quit()
